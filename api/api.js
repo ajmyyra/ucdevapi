d8894cba975d62123fa1cb31cd4a204a7d7d2118
Antti Myyrä <antti.myyra@sigmatic.fi>
2017-05-11 17:28:54 +0300

    Authentication done, next up: API functionality.

--- api/api.js
+++ api/api.js
@@ -10,13 +10,14 @@ export function startServer() {
   server.use(restify.CORS());
   server.use(restify.acceptParser(server.acceptable));
   server.use(restify.bodyParser({ mapParams: true }));
+  server.use(restify.authorizationParser());
 
   server.post('/register', function(req, res, next) {
     var newuser = models.user.build(req.params);
     const userpwd = newuser.generateHash(req.params.password);
     newuser.password = userpwd;
 
-    models.user.findAll({ // TODO promisify correctly to make it shiny and nice.
+    models.user.findAll({ // TODO promisify correctly to make prettier
       where: { username: req.params.username },
       logging: log.debug.bind(log) })
       .then( (user) => {
@@ -44,7 +45,38 @@ export function startServer() {
       });
   })
 
-  // TODO user authentication. Use model.user.validPassword(givenpwd) for validation.
+  server.use(function authenticate(req, res, next) {
+    if (req.authorization.scheme === "Basic") {
+      models.user.findOne({
+        where: { username: req.authorization.basic.username },
+        logging: log.debug.bind(log) })
+        .then( (user) => {
+          if (req.authorization.basic.username === user.username && user.validPassword(req.authorization.basic.password)) {
+            log.debug("Request accepted from user " + user.username);
+            next();
+          }
+          else {
+            throw new Error("Wrong password.");
+          }
+        }).catch( (error) => {
+          log.error("Auth failed for user " + req.authorization.basic.username + ": " + error);
+          res.statusCode = 403;
+          res.json(JSON.parse('{ "error" : { "error_code" : "AUTHENTICATION_FAILED", "error_message" : "Authentication failed using the given username and password." } }'));
+        })
+    }
+    else {
+      log.error("No authentication header provided.");
+      res.statusCode = 401;
+      res.json(JSON.parse('{ "error" : { "error_code" : "AUTHENTICATION_FAILED", "error_message" : "The use of this dev API requires authentication. Create your account at /register." } }'));
+    }
+    
+  })
+
+  // test route for debug
+  server.post('/foo', function(req, res, next) {
+    console.log("Reached /foo");
+    console.log(req.params);
+  })
   // TODO other API functionality. First everything will just return 501 Not Implemented.
 
   server.listen( process.env.PORT || 8080, process.env.IP || "0.0.0.0", () =>
